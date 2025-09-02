816c091fe9cff00b42c19d83fecc5e22c14db627
VinodAlexRaj <vinodalexraj@gmail.com>
2025-09-02 18:43:14 +0800

    v5 Render

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,16 @@
-import { StrapiServerConfig } from '@strapi/strapi';
-
-const serverConfig: StrapiServerConfig = ({ env }) => ({
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
   url: env('STRAPI_ADMIN_BACKEND_URL'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
   },
-  proxy: true,
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
-
-export default serverConfig;
