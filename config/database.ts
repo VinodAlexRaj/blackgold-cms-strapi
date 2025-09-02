10b906b6caa430f0f6639430a4aff5f1f167eb3d
VinodAlexRaj <vinodalexraj@gmail.com>
2025-09-02 16:56:22 +0800

    fix: use Postgres in production on Render

---
+++ config/database.ts
@@ -0,0 +1,10 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+    pool: { min: 0, max: 10 },
+  },
+});
